4df316ca3009c05f17e4a39f815d7d87b7d34761
scdy@gbox.ncf.edu.ph <158818151+alphady12@users.noreply.github.com>
2024-02-18 21:14:38 +0800

    Update customer.js

--- routes/customer.js
+++ routes/customer.js
@@ -9,9 +9,25 @@ const router = express.Router();
 const db = require ('../db');
 const secretKey = require ('../secretkey/secret');
 
+// Middleware to verify JWT token
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(403).json({ message: 'No token provided.' });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(500).json({ message: 'Failed to authenticate token.' });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
 
 // Get all customers
-router.get('/api/customers',  (req, res) => {
+router.get('/api/customers', verifyToken,  (req, res) => {
   try {
     db.query('SELECT * FROM customer', (err, result) =>{
       if(err){
@@ -28,7 +44,7 @@ router.get('/api/customers',  (req, res) => {
 });
 
 // Get a single customer by ID
-router.get('/api/customers/:id', (req, res) => {
+router.get('/api/customers/:id', verifyToken , (req, res) => {
   const { id } = req.params;
   try {
     db.query('SELECT * FROM customer WHERE id = ?', [id], (err, result) => {
@@ -46,7 +62,7 @@ router.get('/api/customers/:id', (req, res) => {
 });
 
 // Create a new customer
-router.post('/api/customers', (req, res) => {
+router.post('/api/customers', verifyToken , (req, res) => {
   const { first_name, last_name, email, phone_number} = req.body;
   try {
     db.query('INSERT INTO customer (first_name, last_name, email, phone_number) VALUES (?, ?, ?, ?)', [first_name, last_name, email, phone_number], (err, result) => {
@@ -64,7 +80,7 @@ router.post('/api/customers', (req, res) => {
 });
 
 // Update a customer (PUT)
-router.put('/api/customers/:id', (req, res) => {
+router.put('/api/customers/:id', verifyToken, (req, res) => {
   const { id } = req.params;
   const { first_name, last_name, email, phone_number } = req.body;
   try {
@@ -82,7 +98,7 @@ router.put('/api/customers/:id', (req, res) => {
   }
 });
 // Delete a customer
-router.delete('/api/customers/:id', (req, res) => {
+router.delete('/api/customers/:id', verifyToken, (req, res) => {
   const { id } = req.params;
   try {
     db.query('DELETE FROM customer WHERE id = ?', [id], (err, result) => {
