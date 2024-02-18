21ccfd8b882adbaf2efaafc63d91619e32906c7a
scdy@gbox.ncf.edu.ph <158818151+alphady12@users.noreply.github.com>
2024-02-18 22:27:10 +0800

    Update customer.js

--- routes/customer.js
+++ routes/customer.js
@@ -9,6 +9,21 @@ const db = require ('../db');
 const secretKey = require ('../secretkey/secret');
 
 
+const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(403).json({ message: 'No token provided.' });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(500).json({ message: 'Failed to authenticate token.' });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
 // Get all customers
 router.get('/api/customers',  (req, res) => {
   try {
